f47c9e42c2a0b5b97e14f611d3b1343c4d51f409
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-06-28 18:39:57 -0300

    working with dummies and spies

--- src/FidelityProgramBundle/Test/Service/FidelityProgramServiceTest.php
+++ src/FidelityProgramBundle/Test/Service/FidelityProgramServiceTest.php
@@ -4,6 +4,7 @@ namespace FidelityProgramBundle\Service;
 
 use FidelityProgramBundle\Repository\PointsRepository;
 use FidelityProgramBundle\Service\FidelityProgramService;
+use MyFramework\LoggerInterface;
 use OrderBundle\Entity\Customer;
 use PHPUnit\Framework\TestCase;
 
@@ -14,37 +15,56 @@ class FidelityProgramServiceTest extends TestCase
      */
     public function shouldSaveWhenReceivePoints()
     {
+        // Mock
         $pointsRepository = $this->createMock(PointsRepository::class);
         $pointsRepository->expects($this->once())
             ->method('save');
 
+        // Stub
         $pointsCalculator = $this->createMock(PointsCalculator::class);
         $pointsCalculator->method("calculatePointsToReceive")
             ->willReturn(100);
-        
-        $fidelityProgramService = new FidelityProgramService($pointsRepository, $pointsCalculator);
+    
+        // Spy
+        $allMessages = [];
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->method("log")
+            ->will($this->returnCallback(
+                function($message) use(&$allMessages) {
+                    $allMessages[] = $message;
+                }
+            ));
 
+        $fidelityProgramService = new FidelityProgramService($pointsRepository, $pointsCalculator, $logger);
+
+        // Dummy
         $customer = $this->createMock(Customer::class);
         $value = 50;
 
         $fidelityProgramService->addPoints($customer, $value);
+        $expectedMessages = [
+            "Checking points for customer",
+            "Customer received points"
+        ];
+        $this->assertEquals($expectedMessages, $allMessages);
     }
 
-    /**
-     * @test
-     */
+    
     public function shouldNotSaveWhenReceiveZeroPoints()
     {
+        // Mock
         $pointsRepository = $this->createMock(PointsRepository::class);
         $pointsRepository->expects($this->never())
             ->method("save");
 
+        // Stub
         $pointsCalculator = $this->createMock(PointsCalculator::class);
         $pointsCalculator->method("calculatePointsToReceive")
             ->willReturn(0);
         
-        $fidelityProgramService = new FidelityProgramService($pointsRepository, $pointsCalculator);
+        $fidelityProgramService = new FidelityProgramService($pointsRepository, $pointsCalculator, $logger);
 
+        // Dummy
         $customer = $this->createMock(Customer::class);
         $value = 50;
 
